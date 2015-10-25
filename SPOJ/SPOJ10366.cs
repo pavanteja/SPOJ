2f594efb52c55809ade10ec4b3775d508f10e122
pavan teja <pavanit1217@hotmail.com>
2015-10-26 03:59:25 +0530

    Play With Dates Accepted

---
+++ SPOJ/SPOJ10366.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Play with dates-CODEIT03
+/// Determination_of_the_day_of_the_week Kraitchik's variation
+/// </summary>
+namespace SPOJ
+{
+    class SPOJ10366
+    {
+        public static void Main()
+        {
+            int K = Convert.ToInt32(Console.ReadLine());
+            string[] l = new string[K];
+            if (K >= 1 && K <= 100)
+            {
+                for (int i = 0; i < K; i++)
+                {
+                    l[i] = Console.ReadLine();
+                }
+
+                int[] mo = { 0, 1, 4, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
+                int[] ce = { 0, 5, 3, 1 };
+                string[] days = { "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+                foreach (string n in l)
+                {
+                    string[] p = n.Split(' ');
+                    if (Convert.ToInt32(p[2]) >= 2012)
+                    {
+
+                        int d = Convert.ToInt32(p[0]);
+                        int m = mo[Convert.ToInt32(p[1])];
+                        int c = ce[Convert.ToInt32(p[2].Substring(0, 2)) % 4];
+                        int s = Convert.ToInt32(p[2].Substring(2, 2));
+                        if (p[1] == "1" || p[1] == "2")
+                        {
+                            s--;
+                        }
+                        int y = ((s / 4) + s) % 7;
+                        int day = (d + m + c + y) % 7;
+                        Console.WriteLine(days[day]);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- SPOJ/SPOJ6.cs
+++ SPOJ/SPOJ6.cs
@@ -10,7 +10,7 @@ namespace SPOJ
 {
     class SPOJ6
     {
-        public static void Main(string[] args)
+        public static void Main6(string[] args)
         {
             int K = Convert.ToInt32(Console.ReadLine());
             string[] l = new string[K];
@@ -20,11 +20,56 @@ namespace SPOJ
             }
             foreach (string n in l)
             {
+                if (n.Contains('+'))
+                {
+                    add(n.Split('+'));
+                }
+                else if (n.Contains('-'))
+                {
+                    sub(n.Split('-'));
+                }
+                else
+                {
+                    mul(n.Split('*'));
+                }
 
 
             }
             Console.ReadLine();
         }
 
+        private static void mul(string[] v)
+        {
+            int l1 = v[0].Length;
+            int l2 = v[1].Length;
+            int rl = (l1 > l2 ? l1 : l2) + 1;
+            char[] result = new char[rl];
+
+            Console.WriteLine(v[0]);
+            Console.WriteLine("+"+v[1]);
+            int i = rl - 1;
+            int carry = 0;
+            while (i >= 0)
+            {
+                int A = Convert.ToInt32(v[0].ToString());
+                int B = Convert.ToInt32(v[1].ToString());
+                int x = A + B + carry;
+                //cn[i] = (x % 10).ToString()[0];
+                //carry = x / 10;
+                i--;
+            }
+
+        }
+
+        private static void sub(string[] v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void add(string[] v)
+        {
+
+
+        }
     }
 }
