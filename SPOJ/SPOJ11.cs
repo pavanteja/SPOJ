030ca8d350f402571f566bb38208cdd904ace12d
pavan teja <pavanit1217@hotmail.com>
2015-10-26 23:34:56 +0530

    Accepted Factorial

--- SPOJ/SPOJ11.cs
+++ SPOJ/SPOJ11.cs
@@ -10,7 +10,7 @@ namespace SPOJ
 {
     class SPOJ11
     {
-        public static void Main()
+        public static void Main11()
         {
 
             string[] l = new string[Convert.ToInt32(Console.ReadLine())];
@@ -18,9 +18,20 @@ namespace SPOJ
             {
                 l[i] = Console.ReadLine();
             }
+            
             foreach (string n in l)
             {
+                int res = 0;
+                ulong num = Convert.ToUInt64(n);
+                double s = 5;
+                int i = 0;
                
+                while(s<=num)
+                {
+                   s= Math.Pow(5,++i);
+                    res += (int)(num / s);
+                }
+                Console.WriteLine(res);
             }
 
             Console.ReadLine();
