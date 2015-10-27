8b805db67efc27cf5a0d4ca975079e35c4a9752f
pavan teja <pavanit1217@hotmail.com>
2015-10-28 00:51:34 +0530

    Small factorials accepted

--- SPOJ/SPOJ24.cs
+++ SPOJ/SPOJ24.cs
@@ -12,115 +12,55 @@ namespace SPOJ
     {
         public static void Main()
         {
-            string[] l = new string[Convert.ToInt32(Console.ReadLine())];
+            int[] l = new int[Convert.ToInt32(Console.ReadLine())];
             for (int i = 0; i < l.Length; i++)
             {
-                l[i] = Console.ReadLine();
+                l[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            foreach (string n in l)
+            foreach (int n in l)
             {
-                string d = mul(n, (Convert.ToInt32(n) - 1) + "");
-
-                //Console.WriteLine("------");
-                Console.WriteLine(d);
+                string x = "1";
+                for (int i = 1; i <= n; i++)
+                {
+                    x = mul(x, i);
+                }
+                Console.WriteLine(x);
 
             }
             Console.ReadLine();
         }
 
-        private static string mul(string A, string B)
+        private static string mul(string x, int n)
         {
-            if (Convert.ToInt32(B) <= 1)
+            char[] ca = x.ToCharArray();
+            int l = ca.Length;
+            int carry = 0;
+            string res = "";
+            while (l > 0)
             {
-                return A;
-            }
-
-            string[] PartialProducts = new string[A.Length];
-            for (int i = A.Length - 1, c = 0; i >= 0; i--, c++)
-            {
-                int dA = Convert.ToInt32(A[i] + "");
-                int carry = 0;
-                string partialproduct = "";
-                for (int j = B.Length - 1; j >= 0; j--)
+                int xp = Convert.ToInt32(ca[l - 1]+"");
+                l--;
+                int pp = (xp * n) + carry;
+                if (pp >= 10)
                 {
-                    int dB = Convert.ToInt32(B[j] + "");
-                    int pp = (dB * dA) + carry;
-
-                    partialproduct = "" + (pp % 10) + partialproduct;
-                    if (pp >= 10)
-                    {
-                        carry = pp / 10;
-                    }
-                    else
-                    {
-                        carry = 0;
-                    }
-                    if (j == 0 && carry > 0)
-                    {
-                        partialproduct = carry + "" + partialproduct;
-                    }
+                    carry = (int)(pp / 10);
+                    res = (pp % 10) + res;
                 }
-                PartialProducts[c] = partialproduct;
-
-            }
-            //for (int i = 0; i < PartialProducts.Length; i++)
-            //{
-            //    for (int j = i + 1; j < PartialProducts.Length; j++)
-            //    {
-            //       Console.Write(" ");
-            //    }
-            //    Console.WriteLine(PartialProducts[i]);
-
-
-
-            //}
-
-            int carry1 = 0;
-            string mres = "";
-            int f = 0;
-            if (PartialProducts.Length == 1)
-            {
-                mres = PartialProducts[0];
-            }
-            else
-            {
-
-                int pi = PartialProducts[0].Length ;
-                for (int i = pi - 1 + PartialProducts.Length - 1; i >= 0; i--)
+                else
                 {
-                    int DA = 0, DB = 0;
-
-                    if (i >= 1 && PartialProducts[0].Length <= i)
-                    {
-                        DA = Convert.ToInt32(PartialProducts[0][i - 1] + "");
-                    }
-
-                    if (f > 0 && i >= 0 && PartialProducts.Length == 2)
-                    {
-                        DB = Convert.ToInt32(PartialProducts[1][i] + "");
-                    }
-                    f = 1;
-                    int sum = (DA + DB + carry1);
-
-
-                    mres = "" + (sum % 10) + mres;
-                    if (sum >= 10)
-                    {
-                        carry1 = sum / 10;
-                    }
-                    else
-                    {
-                        carry1 = 0;
-                    }
-                    if (i == 0 && carry1 > 0)
-                    {
-                        mres = carry1 + "" + mres;
-                    }
+                    carry = 0;
+                    res = pp + res;
                 }
+
+
             }
-            //Console.WriteLine(mres);
-            return mul(mres, (Convert.ToInt32(B) - 1) + "");
+            if (carry > 0)
+            {
+                res = carry + res;
+            }
+
+            return res;
         }
 
     }
